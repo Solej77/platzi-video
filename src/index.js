0fa83a25d82839de6cf1fdf846ad48adc502815e
Solej77 <jose_luispp7@hotmail.com>
2019-10-08 11:33:26 -0500

    Creating reducers

--- src/index.js
+++ src/index.js
@@ -8,21 +8,7 @@ import App from './routes/App';
 const initialState = {
   'user': {},
   'playing': {},
-  'mylist': [
-    {
-      'id': 1,
-      'slug': 'tvshow-2',
-      'title': 'In the Dark',
-      'type': 'Scripted',
-      'language': 'English',
-      'year': 2009,
-      'contentRating': '16+',
-      'duration': 164,
-      'cover': 'http://dummyimage.com/800x600.png/99118E/ffffff',
-      'description': 'Vestibulum ac est lacinia nisi venenatis tristique',
-      'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4',
-    },
-  ],
+  'mylist': [],
   'trends': [
     {
       'id': 2,
