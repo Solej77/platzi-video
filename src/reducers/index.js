4abfc06cd402cc0014dd4282761391f572bf140c
Solej77 <jose_luispp7@hotmail.com>
2019-10-09 11:00:19 -0500

    Validation for Login LogOut

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -16,7 +16,13 @@ const reducer = (state, action) => {
       return {
         ...state,
         user: action.payload,
-      }
+      };
+
+    case 'LOGOUT_REQUEST':
+      return {
+        ...state,
+        user: action.payload,
+      };
 
     default:
       return state;
